8992f22bad365d5fd989283249f8d9949c5200c9
Thomas Klein <thomasklein876@gmail.com>
2024-03-18 16:07:14 +0100

    Fix multi domain websites

--- Plugin/App/Request/PathInfo.php
+++ Plugin/App/Request/PathInfo.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace Opengento\StorePathUrl\Plugin\App\Request;
 
 use Magento\Framework\App\Request\PathInfo as PathInfoSubject;
+use Magento\Framework\App\RequestInterface;
 use Opengento\StorePathUrl\Model\Config;
 use Opengento\StorePathUrl\Service\StorePathFixer;
 
@@ -14,13 +15,14 @@ class PathInfo
 {
     public function __construct(
         private Config $config,
-        private StorePathFixer $storePathFixer
+        private StorePathFixer $storePathFixer,
+        private RequestInterface $request
     ) {}
 
     public function beforeGetPathInfo(PathInfoSubject $subject, string $requestUri, string $baseUrl): array
     {
         if ($this->config->isEnabled()) {
-            $requestUri = $this->storePathFixer->fix($baseUrl, $requestUri);
+            $requestUri = $this->storePathFixer->fix($baseUrl ? $requestUri : $this->request->getUriString(), $requestUri);
         }
 
         return [$requestUri, $baseUrl];
--- Service/StorePathFixer.php
+++ Service/StorePathFixer.php
@@ -9,10 +9,7 @@ namespace Opengento\StorePathUrl\Service;
 use Magento\Store\Api\StoreRepositoryInterface;
 use Magento\Store\Model\Store;
 
-use function parse_url;
 use function str_starts_with;
-
-use const PHP_URL_PATH;
 
 class StorePathFixer
 {
@@ -21,19 +18,15 @@ class StorePathFixer
         private UriUtils $uriUtils
     ) {}
 
+    /**
+     * Replace the request uri path with the store code, if the base url match any of the registered store base url.
+     */
     public function fix(string $baseUrl, string $requestUri): string
     {
         /** @var Store $store */
         foreach ($this->storeRepository->getList() as $store) {
-            if ($store->getId()) {
-                if ($baseUrl === '') {
-                    $path = parse_url($store->getBaseUrl(), PHP_URL_PATH);
-                    if (str_starts_with($requestUri . '/', $path)) {
-                        return $this->uriUtils->replacePathCode($requestUri, $store);
-                    }
-                } elseif (str_starts_with($baseUrl . $requestUri, $store->getBaseUrl())) {
-                    return $this->uriUtils->replacePathCode($requestUri, $store);
-                }
+            if ($store->getId() && str_starts_with($baseUrl, $store->getBaseUrl())) {
+                return $this->uriUtils->replacePathCode($requestUri, $store);
             }
         }
 
