196ab80dfe12ac42a43ba2e731f8051bc4e233a6
Thomas Klein <thomasklein876@gmail.com>
2025-02-10 16:01:13 +0100

    Fix store code resolver when store in path is disabled

--- Plugin/App/Request/StorePathInfoValidator.php
+++ Plugin/App/Request/StorePathInfoValidator.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Opengento\StorePathUrl\Plugin\App\Request;
 
 use Magento\Framework\App\Request\Http;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\UrlInterface;
 use Magento\Store\Api\StoreRepositoryInterface;
@@ -31,13 +32,28 @@ class StorePathInfoValidator
 
     public function beforeGetValidStoreCode(Subject $subject, Http $request, string $pathInfo = ''): array
     {
-        if (++$this->stack === 1 && $this->config->isBaseUrlResolverEnabled()) {
+        if (++$this->stack === 1 && $this->config->isStoreInPath() && $this->config->isBaseUrlResolverEnabled()) {
             $storeCode = $this->resolveStoreCode($request, $pathInfo);
             $pathInfo = $storeCode === '' ? $pathInfo : $storeCode;
         }
         $this->stack--;
 
         return [$request, $pathInfo];
+    }
+    
+    public function afterGetValidStoreCode(Subject $subject, ?string $store, Http $request, string $path = ''): ?string
+    {
+        if (!$this->config->isStoreInPath() && $this->config->isBaseUrlResolverEnabled()) {
+            try {
+                $store = $this->storeRepository->getActiveStoreByCode(
+                    $this->resolveStoreCode($request, $path)
+                )->getCode();
+            } catch (LocalizedException) {
+                $store = null;
+            }
+        }
+
+        return $store;
     }
 
     private function resolveStoreCode(Http $request, string $pathInfo): string
